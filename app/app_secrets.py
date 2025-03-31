53386626ddf752690f6f0536036175e95f1442af
olivier <olivier.boesch@ac-aix-marseille.fr>
2025-03-31 13:38:14 +0200

    temporary app_secrets.py

---
+++ app/app_secrets.py
@@ -0,0 +1,22 @@
+"""
+App sercrets
+
+secret data
+"""
+
+# Login key
+LOGIN_KEY = "EM7M-wLoonBk-83yKcDjK9BCSETMA-W3EhZ1VOdKTt2UVGwiocCktT5w-3Tt-jqJWAnFb-6-qBPi9adVnPDKgIAazhpazPwQhwdu5twexfrOr4oqJtn-WryMeYv32WFd21871Ty4EiqZvMHDIxZ_CO_xP_SfFs_NWIpYPwCgoKQ"
+
+# App secret key
+APP_SECRET_KEY = 'Nzv5FV7pvW_x14uVFp7yNqm9gQQmR2HLjpW1mpsPJcE'
+
+# Db params
+DB_PARAMETERS = {
+    'host': 'localhost',
+    'user': 'nsitheque',
+    'password': 'qjl0R8LFfJo2dGYyR3TwSA',
+    'database': 'NsiTheque'
+}
+
+# Max file size (8Mb)
+MAX_FILE_SIZE = 8 * 1024 * 1024
